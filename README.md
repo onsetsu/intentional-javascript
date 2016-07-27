f72503bb2e686022797a1d14590b3f7b8a5e8a3d
Stefan Lehmann <stefan.lehmann@hpi.de>
2016-07-27 10:49:46 +0200

    setup workflow

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# intentional-javascript
-An experimental environment to test ideas on intentional programming, live programming, and time-traveling
+# intentional-javascript [![Build Status](https://travis-ci.org/onsetsu/intentional-javascript.svg?branch=master)](https://travis-ci.org/composite-scopes/composite-scopes-all)
+An experimental environment to test ideas on intentional programming, live programming, and time-traveling.
+
+## Installation
+
+As npm for Node.js:
+
+```
+$ npm install intentional-javascript --save
+```
+
+Or download the [bundle](https://raw.githubusercontent.com/onsetsu/intentional-javascript/master/dist/intentional-javascript.js) file.
+
+## Building
+
+```
+$ npm run-script build
+```
+
+creates the *bundle* file in the `dist` folder.
+
+## Testing
+
+As npm package:
+
+```
+$ npm test
+```
---
+++ karma.conf.js
@@ -0,0 +1,55 @@
+var webpackConfig = require('./webpack.config');
+webpackConfig.devtool = 'inline-source-map';
+
+module.exports = function (config) {
+    var configuration = {
+        // basePath: '.',
+        browsers: ['chrome_large'],
+        customLaunchers: {
+            chrome_large: {
+                base: 'Chrome',
+                flags: [
+                    '--window-size=1100,600',
+                    '--window-position=-0,0',
+                    '--no-sandbox'
+                ]
+            },
+            ChromeCanary_Travis_CI_large_no_sandbox: {
+                base: 'ChromeCanary',
+                flags: [
+                    '--window-size=1100,600',
+                    '--window-position=-0,0',
+                    '--no-sandbox'
+                ]
+            }
+        },
+        files: [
+            'test/runner.js'
+        ],
+        plugins: [
+            'karma-chrome-launcher',
+            'karma-chai',
+            'karma-sinon',
+            'karma-mocha',
+            'karma-sourcemap-loader',
+            'karma-webpack',
+            'karma-mocha-reporter'
+        ],
+        frameworks: [ 'chai', 'mocha', 'sinon' ],
+        preprocessors: {
+            'test/**/*': ['webpack', 'sourcemap'],
+            'src/**/*': ['webpack', 'sourcemap']
+        },
+        reporters: [ 'progress', 'mocha' ],
+        singleRun: false,
+        webpack: webpackConfig,
+        webpackServer: {
+            noInfo: true
+        }
+    };
+    if (process.env.TRAVIS) {
+        configuration.browsers = ['ChromeCanary_Travis_CI_large_no_sandbox'];
+    }
+
+    config.set(configuration);
+};
